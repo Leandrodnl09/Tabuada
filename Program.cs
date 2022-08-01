82bf77908060140c68c21629766e30217fa62798
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-01 15:26:31 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Exercício do site URI "Tabuada"
+// Programa básico, aqui iremos usar o "for" para fazermos uma tabuada!
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i + " X " + N + " = " + i * N);
+}
